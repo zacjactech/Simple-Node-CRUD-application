728a0a39f5f6ee12fea0e25cf06386862f76a2e4
zacjactech <cb.zacjac@gmail.com>
2024-10-27 00:10:49 +0000

    update addData, query and retrive,js

--- queryDB.js
+++ queryDB.js
@@ -1,14 +1,31 @@
 import fs from "fs";
 
-let info = [];
-    if (fs.existsSync("db.json")) {
-      fs.readFile("db.json", function (err, data) {
-        if (err) {
-          console.log(err);
+export default async function queryDB(externalFunction) {
+    try {
+      let info = [];
+  
+      if (fs.existsSync("db.json")) {
+        await fs.readFile("db.json", function (err, data) {
+          info = JSON.parse(data.toString());
+          console.log(info);
+  
+          if (err) {
+            console.log(err);
+            return;
+          }
+  
+          if (externalFunction && !err) {
+            externalFunction(info);
+            return;
+          }
+        });
+      } else {
+        if (externalFunction) {
+          externalFunction(info);
+          return;
         }
-        info = JSON.parse(data.toString());
-        console.log(info);
-      });
-    } else {
-      console.log("No data available!");
+      }
+    } catch (error) {
+      console.error(`Something Happened: ${error.message}`);
     }
+  }
